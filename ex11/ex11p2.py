4ec2d8e2c5f8b8cc8a78b75e5194f5e2adb9aa52
Nuno Gonçalves <nunombg@gmail.com>
2020-12-12 15:44:50 +0000

    Add solution for day 11 part 2

--- ex11/ex11p2.py
+++ ex11/ex11p2.py
@@ -1,60 +1,69 @@
 import copy
 
-current = []
-
-with open("input.txt", "r") as f:
-	for l in f.readlines():
-		current += [[c for c in l.strip()]]
+EMPTY = "L"
+OCCUPIED = "#"
+FLOOR = "."
+vectors = [(1,0),(-1,0),(0,1),(0,-1),(1,1),(1,-1),(-1,1),(-1,-1)]
 
 def pretty_print(layout):
 	for line in layout:
 		print(line)
 	print("====================================")
 
+def visible_occupied(layout, pos):
+	for vector in vectors:
+		x = pos[1]
+		y = pos[0]
+		while 0 <= y <= len(layout)-1 and 0 <= x <= len(layout[0])-1:
+			if layout[y][x] != FLOOR and not (y == pos[0] and x == pos[1]):
+				if layout[y][x] == OCCUPIED:
+					return True
+				else:
+					break
+			x += vector[0]
+			y += vector[1]
+
+	return False
+
+def count_visible_occupied(layout, pos):
+	count = 0
+	for vector in vectors:
+		x = pos[1]
+		y = pos[0]
+
+		while 0 <= y <= len(layout)-1 and 0 <= x <= len(layout[0])-1:
+			if layout[y][x] != FLOOR and not (y == pos[0] and x == pos[1]):
+				if layout[y][x] == OCCUPIED:
+					count += 1
+					if count == 5:
+						return True
+				break
+			x += vector[0]
+			y += vector[1]
+
+	return False
+
+current = []
 new = []
 
-EMPTY = "L"
-OCCUPIED = "#"
-FLOOR = "."
+with open("input.txt", "r") as f:
+	for l in f.readlines():
+		current += [[c for c in l.strip()]]
 
 while True:
 	new = copy.deepcopy(current)
 
 	for i in range(len(current)):
 		for j in range(len(current[i])):
-			if current[i][j] == EMPTY:
-				if not ((i-1 >= 0 and current[i-1][j] == OCCUPIED) 
-					or (i+1 <= len(current)-1 and current[i+1][j] == OCCUPIED)
-					or (j-1 >= 0 and current[i][j-1] == OCCUPIED)
-					or (j+1 <= len(current[i])-1 and current[i][j+1] == OCCUPIED) 
-					or (j+1 <= len(current[i])-1 and i+1 <= len(current)-1 and current[i+1][j+1] == OCCUPIED) 
-					or (j-1 >= 0 and i+1 <= len(current)-1 and current[i+1][j-1] == OCCUPIED) 
-					or (j+1 <= len(current[i])-1 and i-1 >=0 and current[i-1][j+1] == OCCUPIED)
-					or (j-1 >= 0 and i-1 >= 0 and current[i-1][j-1] == OCCUPIED)):
-						new[i][j] = OCCUPIED
+			if current[i][j] == EMPTY and not visible_occupied(current, (i,j)):
+				new[i][j] = OCCUPIED
 
-			if current[i][j] == OCCUPIED:
-				total = 0
-				if (i-1 >= 0 and current[i-1][j] == OCCUPIED):
-					total+=1 
-				if (i+1 <= len(current)-1 and current[i+1][j] == OCCUPIED):
-					total+=1
-				if (j-1 >= 0 and current[i][j-1] == OCCUPIED):
-					total+=1
-				if (j+1 <= len(current[i])-1 and current[i][j+1] == OCCUPIED):
-					total+=1 
-				if (j+1 <= len(current[i])-1 and i+1 <= len(current)-1 and current[i+1][j+1] == OCCUPIED): 
-					total+=1
-				if (j-1 >= 0 and i+1 <= len(current)-1 and current[i+1][j-1] == OCCUPIED):
-					total+=1
-				if (j+1 <= len(current[i])-1 and i-1 >=0 and current[i-1][j+1] == OCCUPIED):
-					total+=1
-				if (j-1 >= 0 and i-1 >= 0 and current[i-1][j-1] == OCCUPIED):
-					total+=1
+			if current[i][j] == OCCUPIED and count_visible_occupied(current, (i,j)):
+				new[i][j] = EMPTY
 
-				if total>=5:
-					new[i][j] = EMPTY
-
+	# pretty_print(current)
+	# pretty_print(new)
+	# input()
 	if new == current:
 		break
 
